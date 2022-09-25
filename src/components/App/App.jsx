64e2be98c936eb9ea7b2546a9d3f4d8313be71ff
Andrey <artist_hire@ukr.net>
2022-09-25 14:39:18 +0300

    Add global components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+import Container from 'components/Container';
+import FormInput from 'components/FormInput';
+import Button from 'components/Button';
+
 const App = () => {
-    return (
-      <div></div>
-  )  
-}
+  const [value, setValue] = useState('');
+
+  return (
+    <Container>
+      <FormInput
+        label={{ id: 'bookTitle', text: 'Book title' }}
+        input={{
+          value,
+          type: 'text',
+          onChange: ({ target }) => setValue(target.value),
+        }}
+        errorMessage=""
+      />
+
+      <Button variant="outline" type="submit">
+        Add
+      </Button>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/components/FormInput/FormInput.jsx
@@ -0,0 +1,58 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import s from './FormInput.module.scss';
+
+function FormInput({
+  label,
+  input,
+  errorMessage = '',
+  modifClasses = {},
+  children,
+}) {
+  return (
+    <div className={classNames(s.wrapper, modifClasses.wrapper)}>
+      <label
+        htmlFor={label.id}
+        className={classNames(s.label, modifClasses.label)}
+      >
+        {label.text}
+      </label>
+      <input
+        id={label.id}
+        className={
+          errorMessage
+            ? classNames(s.input, s.invalid, modifClasses.input)
+            : classNames(s.input, modifClasses.input)
+        }
+        type={input?.type ?? 'text'}
+        placeholder={input?.placeholder ?? '...'}
+        onChange={input.onChange}
+        value={input.value}
+      />
+      {children}
+      {errorMessage ? <span className={s.error}>{errorMessage}</span> : null}
+    </div>
+  );
+}
+
+FormInput.propTypes = {
+  label: PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+  input: PropTypes.exact({
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+  }).isRequired,
+  errorMessage: PropTypes.string,
+  modifClasses: PropTypes.exact({
+    wrapper: PropTypes.string,
+    label: PropTypes.string,
+    input: PropTypes.string,
+  }),
+  children: PropTypes.node,
+};
+
+export default FormInput;
