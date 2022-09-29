ff46336a5e00c25c0d869a2266f40347a06c1423
Marian Kuzyk <mariankuzyk@gmail.com>
2022-09-30 01:17:02 +0300

    add page with google answer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import NotFound from 'pages/NotFound';
 import AppBar from 'components/Header/AppBar';
 import Library from 'pages/Library';
 import TrainingPage from 'pages/TrainingPage';
+import GoogleAnswerPage from 'pages/GoogleAnswerPage';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/answer-google" element={<GoogleAnswerPage />} />
         <Route path="/" element={<AppBar />}>
           <Route index element={<Library />} />
           <Route path="/training" element={<TrainingPage />} />
