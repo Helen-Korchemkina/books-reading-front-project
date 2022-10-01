84d5479178932ac8f34a6fd621e26e3d1e940a21
Leevorum <di4111ua@gmail.com>
2022-10-01 19:55:03 +0300

    merge and fixes

--- src/pages/InfoPageMobile/InfoPageMobile.jsx
+++ src/pages/InfoPageMobile/InfoPageMobile.jsx
@@ -1,5 +1,5 @@
-import Button from 'components/Button';
-import Container from 'components/Container';
+import Button from 'components/common/Button';
+import Container from 'components/common/Container';
 import { Link } from 'react-router-dom';
 import BooksReadingAdvantages from 'components/BooksReadingAdvantages/BooksReadingAdvantages';
 import s from './InfoPage.module.scss';
