da64b87074f2786f7618b5710651d84c50c50950
PawelYarko <pawelyarko@gmail.com>
2022-10-02 15:35:12 +0200

    small fix mobile training

--- src/components/Training/BookList/Book/Book.jsx
+++ src/components/Training/BookList/Book/Book.jsx
@@ -1,9 +1,19 @@
+import { MdOutlineMenuBook } from 'react-icons/md';
+import classNames from 'classnames';
 import s from './Book.module.css';
 
 const Book = () =>{
     return(
     <>
     <li>
+        <div className={s.listIcon}>
+            <MdOutlineMenuBook
+            className={classNames({
+                [s.icon]: true,
+            })}
+            />
+            <h2 className={s.listTitle}>...</h2>
+        </div>
         <p className={s.item}>
             <span classname={s.name}>Author:</span> 
             <span className={s.desc}>...</span>
