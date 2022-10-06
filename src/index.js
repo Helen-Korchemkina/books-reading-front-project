29e388b37f770e84c7e412f9b0faac504a9a62a5
Marian <90156656+mrkuzyk@users.noreply.github.com>
2022-10-06 13:08:09 +0300

    update BrowserRouter

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
     <ToastContainer />
