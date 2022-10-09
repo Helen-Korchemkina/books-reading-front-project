1af4e053a06facd6391059e0dab9fb1f6501703f
AButsai <butsai@ukr.net>
2022-10-09 11:57:47 +0300

    delite book from training

--- src/components/Training/BooksTable/BooksTable.jsx
+++ src/components/Training/BooksTable/BooksTable.jsx
@@ -1,58 +1,92 @@
 import { MdOutlineMenuBook } from 'react-icons/md';
 import { MdDeleteOutline } from 'react-icons/md';
-import Button from 'components/common/Button';
+import Media from 'react-media';
 import classNames from 'classnames';
+import BooksTableMobile from '../BooksTableMobile';
 import s from './BooksTable.module.scss';
-import { useRemoveBookMutation } from '../../../redux/books/books-api';
+import { useUpdateStatusBookMutation } from '../../../redux/books/books-api';
 
 const BooksTable = ({ books = [] }) => {
-  const [removeBook] = useRemoveBookMutation();
+  const [updateStatusBook] = useUpdateStatusBookMutation();
+
+  const handleClickDeliteBookFromTrening = id => {
+    try {
+      updateStatusBook({
+        id,
+        status: 'Going to read',
+        isReadBook: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <table className={s.table}>
-        <thead>
-          <tr className={s.headerRow}>
-            <th>Book title</th>
-            <th>Author</th>
-            <th>Year</th>
-            <th>Pages</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.map(({ _id, title, author, countOfPages, releaseDate }) => (
-            <tr key={_id} className={s.bodyRow}>
-              <td className={s.bodyRowTitle}>
-                <MdOutlineMenuBook
-                  className={classNames({
-                    [s.icon]: true,
-                  })}
-                />
-                <span className={s.title}>{title}</span>
-              </td>
-              <td className={s.inlineTitle}>
-                <span className={s.subtitle}>Author:</span>
-                {author}
-              </td>
-              <td className={s.inlineTitle}>
-                <span className={s.subtitle}>Year:</span>
-                {releaseDate}
-              </td>
-              <td className={s.inlineTitle}>
-                {title !== '...' && (
-                  <MdDeleteOutline
-                    className={classNames({
-                      [s.iconDelete]: true,
-                    })}
-                    onClick={() => removeBook(_id)}
-                  />
+      <Media queries={{ small: { maxWidth: 767 } }}>
+        {matches =>
+          matches.small && (
+            <BooksTableMobile
+              books={books}
+              onDel={handleClickDeliteBookFromTrening}
+            />
+          )
+        }
+      </Media>
+      <Media queries={{ small: { minWidth: 768 } }}>
+        {matches =>
+          matches.small && (
+            <table className={s.table}>
+              <thead>
+                <tr className={s.headerRow}>
+                  <th>Book title</th>
+                  <th>Author</th>
+                  <th>Year</th>
+                  <th>Pages</th>
+                </tr>
+              </thead>
+              <tbody>
+                {books.map(
+                  ({ _id, title, author, countOfPages, releaseDate }) => (
+                    <tr key={_id} className={s.bodyRow}>
+                      <td className={s.bodyRowTitle}>
+                        <MdOutlineMenuBook
+                          className={classNames({
+                            [s.icon]: true,
+                          })}
+                        />
+                        <span className={s.title}>{title}</span>
+                      </td>
+                      <td className={s.inlineTitle}>
+                        <span className={s.subtitle}>Author:</span>
+                        {author}
+                      </td>
+                      <td className={s.inlineTitle}>
+                        <span className={s.subtitle}>Year:</span>
+                        {releaseDate}
+                      </td>
+                      <td className={s.inlineTitle}>
+                        {title !== '...' && (
+                          <MdDeleteOutline
+                            className={classNames({
+                              [s.iconDelete]: true,
+                            })}
+                            onClick={() =>
+                              handleClickDeliteBookFromTrening(_id)
+                            }
+                          />
+                        )}
+                        <span className={s.subtitle}>Pages:</span>
+                        {countOfPages}
+                      </td>
+                    </tr>
+                  )
                 )}
-                <span className={s.subtitle}>Pages:</span>
-                {countOfPages}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </tbody>
+            </table>
+          )
+        }
+      </Media>
     </>
   );
 };
